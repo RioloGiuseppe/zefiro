fc5a9423560eb848b15b96c60b0e5be4f8ee97c2
Giuseppe Riolo <riolo@innovactive.it>
2017-09-14 19:18:40 +0200

    Aggiunto mongo js e routes per file statici

---
+++ index.js
@@ -0,0 +1,73 @@
+// Importo le dipendenze necessarie per l'esecuzione del codice
+
+var MongoClient = require('mongodb').MongoClient;
+const Hapi = require('hapi');
+const si = require('systeminformation');
+var Path = require('path');
+
+// Definisco la scringa di connessione al db mongo (indirizzo:porta/database)
+var url = 'mongodb://localhost:27017/zefiro_db';
+
+var error = false;
+
+// Definisco la variable per il server http hapi
+const server = new Hapi.Server();
+
+// Prepraro il riferimento all'oggetto per mongo db
+var db = {};
+
+// Avvio la connessione al database
+MongoClient.connect(url, (_err, _db) => {
+    if (_err) error = true;
+    db = _db;
+});
+
+// Definisco la funzione async che 
+//  > legge le info di carico di sistema
+//  > le scrive sulla collection 'system_status'
+async function writeDB(callback) {
+    var load = await si.currentLoad();
+    db.collection('system_status').insertOne({
+        "Total": load.currentload,
+        "User": load.currentload_user,
+        "System": load.currentload_system
+    }, callback);
+}
+
+// Imposto l'endpoint del server
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+server.register(require('inert'), (err) => {
+    // Configuro le rotte (routes)
+    server.route({
+        method: 'GET',
+        path: '/currentLoad',
+        handler: async function(request, reply) {
+            return reply(await si.currentLoad());
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            directory: {
+                path: './public',
+                listing: false,
+                index: true
+            }
+        }
+    });
+
+
+    // avvio la lettura info/ scrittura db con periodicità un sec
+    //setInterval(writeDB, 1000);
+
+    // Avvio il server http
+    server.start((err) => {
+        if (err) error = true;
+        console.log('Server running at:', server.info.uri);
+    });
+});
