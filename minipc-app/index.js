031c048e602d889177a825fada67b72536545cd9
Giuseppe Riolo <riolo@innovactive.it>
2017-09-19 23:20:58 +0200

    Aggiunta app per minipc

---
+++ minipc-app/index.js
@@ -0,0 +1,75 @@
+// Importo le dipendenze necessarie per l'esecuzione del codice
+// Web-server
+const Hapi = require('hapi');
+// Systeminformation
+const si = require('systeminformation');
+// File statici (html/js/css)
+var Path = require('path');
+// Gestore del bus i2c
+var i2cBus = require('i2c-bus').openSync(0);
+// Port expander
+var PCF8574 = require('pcf8574').PCF8574;
+// Indirizzo sul bus del port expander (Datasheet)
+var pcf_addr = 0x38;
+var pcf = new PCF8574(i2cBus, pcf_addr, true);
+// Definisco i pin 4..7 uscite, spente, non invertenti
+pcf.outputPin(4, false, false);
+pcf.outputPin(5, false, false);
+pcf.outputPin(6, false, false);
+pcf.outputPin(7, false, false);
+// Spengo i pins 4..7
+pcf.setPin(4, false)
+    .then(() => pcf.setPin(5, false))
+    .then(() => pcf.setPin(6, false))
+    .then(() => pcf.setPin(7, false));
+
+// Costruisco il server hapi
+const server = new Hapi.Server();
+
+// Imposto l'endpoint del server
+server.connection({ host: '0.0.0.0', port: 8000 });
+// Configuro le rotte (routes)
+server.register(require('inert'), (err) => {
+    // Orriene il carico corrente della cpu
+    server.route({
+        method: 'GET',
+        path: '/currentLoad',
+        handler: async function(request, reply) {
+            var _cpu = await si.currentLoad();
+            var ret = {
+                total: _cpu.currentload,
+                user: _cpu.currentload_user,
+                system: _cpu.currentload_system
+            }
+            return reply(ret);
+        }
+    });
+    // Controlla i bottoni
+    server.route({
+        method: 'GET',
+        path: '/button/{led}',
+        handler: async function(request, reply) {
+            var led = encodeURIComponent(request.params.led);
+            pcf.setPin(Number.parseInt(led) + 4);
+            return reply();
+        }
+    });
+    // Gestisce i file statici
+    server.route({
+        method: 'GET',
+        path: '/{path*}',
+        handler: {
+            directory: {
+                path: './public',
+                listing: false,
+                index: true
+            }
+        }
+    });
+
+    // Avvio il server http
+    server.start((err) => {
+        if (err) error = true;
+        console.log('Server running at:', server.info.uri);
+    });
+});
